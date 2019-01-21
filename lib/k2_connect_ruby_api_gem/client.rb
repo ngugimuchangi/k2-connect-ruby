0bbb1fc2af8290d788a56e8dc51e6cb34f160f1d
DavidKar1uk1 <David.mwangi@kopokop.com>
2019-01-21 16:28:01 +0300

    Version 0.4.2. Testing K2 resourceId.

--- lib/k2_connect_ruby_api_gem/client.rb
+++ lib/k2_connect_ruby_api_gem/client.rb
@@ -5,10 +5,10 @@ module K2ConnectRubyApiGem
     end
 
     # This is the method for get parsing the json response
-    def parse_it(json)
+    def parse_it()
 
       # With YAJL
-      # json = File.open('/home/k2-engineering-03/RubymineProjects/k2_exported_JSON.json', 'r')
+      json = File.open('/home/k2-engineering-03/RubymineProjects/k2_exported_JSON.json', 'r')
       parser = Yajl::Parser.new
       hash = parser.parse(json)
 
@@ -16,10 +16,10 @@ module K2ConnectRubyApiGem
       # comparison_signature="c839d24ff8a2ec26550d435cf69891347b63aae8"
 
       hash.extend Hashie::Extensions::DeepFind
-      authorize_it(hash.deep_select("body").to_s, hash.deep_select("X-KopoKopo-Signature").to_s)
+      authorize_it(hash.deep_select("resourceId").to_s, hash.deep_select("X-KopoKopo-Signature").to_s)
       # puts ("\n\nThis is the Hash Body: \t #{hash}")
       # L8r change this to be X-KopoKopo-Signature
-      # puts ("\n\nThis is the Hash Header: \t #{hash.deep_select("header")}")
+      # puts ("\n\nThis is the Hash Body: \t #{hash.deep_select("body")}")
 
     end
 
@@ -28,7 +28,7 @@ module K2ConnectRubyApiGem
       digest = OpenSSL::Digest.new('sha1')
       hmac = OpenSSL::HMAC.hexdigest(digest, secret_key, message_body)
       # puts("\n\n This is the trial HMAC hexdigest #{hmac}")
-      puts("\n\n Message Body: #{message_body}, \n\nX-K2-Signature: #{comparison_signature}")
+      puts("\n\n Message resourceId: #{message_body} \n\nX-K2-Signature: #{comparison_signature}")
       puts(hmac.eql?(comparison_signature))
     end
   end
