95cfc1840e51374d206dcf5df6ca24c895487f12
DavidKar1uk1 <David.mwangi@kopokop.com>
2019-01-23 12:45:04 +0300

    Version 0.6.6. Simple misspelling issue.

--- lib/k2_connect_ruby_api_gem/client.rb
+++ lib/k2_connect_ruby_api_gem/client.rb
@@ -37,7 +37,7 @@ module K2ConnectRubyApiGem
       hash_body = Yajl::Parser.parse(the_res.request.body.string.to_json)
       # The Response Header
       hash_header = Yajl::Parser.parse(the_res.request.headers.env.select{|k, _| k =~ /^HTTP_/}.to_json)
-      hash_r.extend Hashie::Extensions::DeepFind
+      hash_header.extend Hashie::Extensions::DeepFind
       # puts ("The Response without JSON: #{the_res}\n\n The Response with an attempted Hash #{hash_r}")
       authorize_it(hash_body.to_s, hash_header.deep_select("HTTP_X_KOPOKOPO_SIGNATURE").to_s)
     end
