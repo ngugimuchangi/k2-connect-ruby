dccb75bff70472250feeabccc3d714d78e9ed913
DavidKar1uk1 <David.mwangi@kopokop.com>
2019-01-23 10:56:12 +0300

    Version 0.5.11. Now Changed how the K2 Signature is fetched.

--- lib/k2_connect_ruby_api_gem/client.rb
+++ lib/k2_connect_ruby_api_gem/client.rb
@@ -13,15 +13,15 @@ module K2ConnectRubyApiGem
       hash_h = Yajl::Parser.parse(req_headers)
 
       hash_h.extend Hashie::Extensions::DeepFind
-      authorize_it(hash_b.to_s, hash_h.deep_select("HTTP_X_KOPOKOPO_SIGNATURE").to_s)
+      authorize_it(hash_b.to_s, hash_h.deep_select(:HTTP_X_KOPOKOPO_SIGNATURE).to_s)
     end
 
     # Method for comparing the HMAC and with HTTP_X_KOPOKOPO_SIGNATURE
     def authorize_it(message_body, comparison_signature)
       digest = OpenSSL::Digest.new('sha256')
       hmac = OpenSSL::HMAC.hexdigest(digest, @secret_key, message_body)
-      puts("\n\nMessage Body: #{message_body}\n\nX-K2-Signature: #{JSON.parse(comparison_signature).join(', ')}\n\n The HMAC hash: #{hmac}")
-      puts(hmac.to_s.eql?(JSON.parse(comparison_signature).join(', ')))
+      puts("\n\nMessage Body: #{message_body}\n\nX-K2-Signature: #{comparison_signature}\n\n The HMAC hash: #{hmac}")
+      puts(hmac.to_s.eql?(comparison_signature))
     end
   end
 end
