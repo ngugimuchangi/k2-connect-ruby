5f42adac0799a528cf4def92c52e3fd1694c4640
DavidKar1uk1 <David.mwangi@kopokop.com>
2019-01-23 12:55:16 +0300

    Version 0.6.7. Added Method print.

--- lib/k2_connect_ruby_api_gem/client.rb
+++ lib/k2_connect_ruby_api_gem/client.rb
@@ -37,9 +37,11 @@ module K2ConnectRubyApiGem
       hash_body = Yajl::Parser.parse(the_res.request.body.string.to_json)
       # The Response Header
       hash_header = Yajl::Parser.parse(the_res.request.headers.env.select{|k, _| k =~ /^HTTP_/}.to_json)
-      hash_header.extend Hashie::Extensions::DeepFind
-      # puts ("The Response without JSON: #{the_res}\n\n The Response with an attempted Hash #{hash_r}")
-      authorize_it(hash_body.to_s, hash_header.deep_select("HTTP_X_KOPOKOPO_SIGNATURE").to_s)
+      # The Response Method
+      hash_method = Yajl::Parser.parse(the_res.request.method.to_json)
+      # hash_header.extend Hashie::Extensions::DeepFind
+      puts ("\n\n The Response Method: #{hash_method}")
+      # authorize_it(hash_body.to_s, hash_header.deep_select("HTTP_X_KOPOKOPO_SIGNATURE").to_s)
     end
   end
 end
