1d3f19c23c910c196d9c1ee72448434f24e6e832
DavidKar1uk1 <David.mwangi@kopokop.com>
2019-01-24 13:06:31 +0300

    Version 0.7.28. Remove the quotes and brackets.

--- lib/k2_connect_ruby_api_gem/client.rb
+++ lib/k2_connect_ruby_api_gem/client.rb
@@ -37,7 +37,7 @@ module K2ConnectRubyApiGem
 
     def assign_req_elements(the_req_body)
       the_req_body.extend Hashie::Extensions::DeepFind
-      puts("\n\nTopic:\t#{the_req_body["topic"]}\nReference:\t#{the_req_body.deep_select("reference").to_s}\nMSISDN:\t#{the_req_body["msisdn"]}\nAmount:\t#{the_req_body["amount"]}\nCurrency:\t#{the_req_body["currency"]}\nTill Number:\t#{the_req_body["till_number"]}\nSystem:\t#{the_req_body["system"]}\nSender First Name:\t#{the_req_body["sender_first_name"]}\nSender Middle Name:\t#{the_req_body["sender_middle_name"]}\nSender Last Name:\t#{the_req_body["sender_last_name"]}\n")
+      puts("\n\nTopic:\t#{the_req_body["topic"]}\nReference:\t#{JSON.parse(the_req_body.deep_select("reference").to_s).join(', ')}\nMSISDN:\t#{the_req_body["msisdn"]}\nAmount:\t#{the_req_body["amount"]}\nCurrency:\t#{the_req_body["currency"]}\nTill Number:\t#{the_req_body["till_number"]}\nSystem:\t#{the_req_body["system"]}\nSender First Name:\t#{the_req_body["sender_first_name"]}\nSender Middle Name:\t#{the_req_body["sender_middle_name"]}\nSender Last Name:\t#{the_req_body["sender_last_name"]}\n")
     end
   end
 end
