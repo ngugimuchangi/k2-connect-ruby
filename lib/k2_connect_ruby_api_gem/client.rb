f6bf5d715d143bfce6d913d917bea8389e01a1d6
DavidKar1uk1 <David.mwangi@kopokop.com>
2019-01-24 11:13:11 +0300

    Version 0.7.4. Looping through the Hashes with to_s method.

--- lib/k2_connect_ruby_api_gem/client.rb
+++ lib/k2_connect_ruby_api_gem/client.rb
@@ -24,7 +24,7 @@ module K2ConnectRubyApiGem
       # puts ("\n\n The Response Method: #{hash_method}")
       if hash_method.eql?("POST")
         if authorize_it(hash_body.to_s, hash_header.deep_select("HTTP_X_KOPOKOPO_SIGNATURE").to_s)
-          assign_req_elements(hash_body) and return
+          assign_req_elements(hash_body.to_s) and return
           return 200
         else
           return 401
