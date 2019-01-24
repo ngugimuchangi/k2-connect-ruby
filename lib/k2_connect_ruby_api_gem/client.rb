27af719f6546de5c274ab536976acb908a7bae7a
DavidKar1uk1 <David.mwangi@kopokop.com>
2019-01-24 13:13:58 +0300

    Version 0.7.29. Final Edit.

--- lib/k2_connect_ruby_api_gem/client.rb
+++ lib/k2_connect_ruby_api_gem/client.rb
@@ -37,7 +37,7 @@ module K2ConnectRubyApiGem
 
     def assign_req_elements(the_req_body)
       the_req_body.extend Hashie::Extensions::DeepFind
-      puts("\n\nTopic:\t#{the_req_body["topic"]}\nReference:\t#{JSON.parse(the_req_body.deep_select("reference").to_s).join(', ')}\nMSISDN:\t#{the_req_body["msisdn"]}\nAmount:\t#{the_req_body["amount"]}\nCurrency:\t#{the_req_body["currency"]}\nTill Number:\t#{the_req_body["till_number"]}\nSystem:\t#{the_req_body["system"]}\nSender First Name:\t#{the_req_body["sender_first_name"]}\nSender Middle Name:\t#{the_req_body["sender_middle_name"]}\nSender Last Name:\t#{the_req_body["sender_last_name"]}\n")
+      puts("\n\nTopic:\t#{the_req_body["topic"]}\nReference:\t#{JSON.parse(the_req_body.deep_select("reference").to_s).join(', ')}\nMSISDN:\t#{JSON.parse(the_req_body.deep_select("sender_msisdn").to_s).join(', ')}\nAmount:\t#{JSON.parse(the_req_body.deep_select("amount").to_s).join(', ')}\nCurrency:\t#{JSON.parse(the_req_body.deep_select("currency").to_s).join(', ')}\nTill Number:\t#{JSON.parse(the_req_body.deep_select("till_number").to_s).join(', ')}\nSystem:\t#{JSON.parse(the_req_body.deep_select("system").to_s).join(', ')}\nSender First Name:\t#{JSON.parse(the_req_body.deep_select("sender_first_name").to_s).join(', ')}\nSender Middle Name:\t#{JSON.parse(the_req_body.deep_select("sender_middle_name").to_s).join(', ')}\nSender Last Name:\t#{JSON.parse(the_req_body.deep_select("sender_last_name").to_s).join(', ')}\n")
     end
   end
 end
--- lib/k2_connect_ruby_api_gem/version.rb
+++ lib/k2_connect_ruby_api_gem/version.rb
@@ -1,3 +1,3 @@
 module K2ConnectRubyApiGem
-  VERSION = "0.7.28"
+  VERSION = "0.7.29"
 end
