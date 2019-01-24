653d9342be16da91ebef7925e48e5ed99d52fe15
DavidKar1uk1 <David.mwangi@kopokop.com>
2019-01-24 12:26:42 +0300

    Version 0.7.16. Implement test within Lib.

--- lib/k2_connect_ruby_api_gem/client.rb
+++ lib/k2_connect_ruby_api_gem/client.rb
@@ -16,14 +16,13 @@ module K2ConnectRubyApiGem
     def parse_it_whole(the_req)
       # The Response Body
       hash_body = Yajl::Parser.parse(the_req.body.string.to_json)
-      h_b = JSON.parse(the_req.body.string.as_json).to_s
+      h_b = JSON.parse(request.body.string).as_json
       # The Response Header
       hash_header = Yajl::Parser.parse(the_req.headers.env.select{|k, _| k =~ /^HTTP_/}.to_json)
       # The Response Method
       hash_method = Yajl::Parser.parse(the_req.method.to_json)
       hash_header.extend Hashie::Extensions::DeepFind
-      h_b.extend Hashie::Extensions::DeepFind
-      puts(h_b.deep_select("topic").to_s)
+      puts ("Topic:\t#{h_b["topic"]}")
       if hash_method.eql?("POST")
         if authorize_it(hash_body.to_s, hash_header.deep_select("HTTP_X_KOPOKOPO_SIGNATURE").to_s)
           # assign_req_elements(Yajl::Parser.parse(the_req.body.string.as_json)) and return
--- lib/k2_connect_ruby_api_gem/version.rb
+++ lib/k2_connect_ruby_api_gem/version.rb
@@ -1,3 +1,3 @@
 module K2ConnectRubyApiGem
-  VERSION = "0.7.17"
+  VERSION = "0.7.18"
 end
