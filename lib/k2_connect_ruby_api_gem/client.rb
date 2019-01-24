5631afe8b55ffd19a8c3d4a9695ceb75695d6e5f
DavidKar1uk1 <David.mwangi@kopokop.com>
2019-01-24 14:56:20 +0300

    Version 0.7.30. Upto the Environment Variables have been set.

--- lib/k2_connect_ruby_api_gem/client.rb
+++ lib/k2_connect_ruby_api_gem/client.rb
@@ -16,8 +16,6 @@ module K2ConnectRubyApiGem
     def parse_it_whole(the_req)
       # The Response Body
       hash_body = Yajl::Parser.parse(the_req.body.string.to_json)
-      test = JSON.parse(the_req.body.string).as_json
-      puts ("Test Topic:\t#{test["topic"]}")
       # The Response Header
       hash_header = Yajl::Parser.parse(the_req.headers.env.select{|k, _| k =~ /^HTTP_/}.to_json)
       # The Response Method
@@ -37,7 +35,7 @@ module K2ConnectRubyApiGem
 
     def assign_req_elements(the_req_body)
       the_req_body.extend Hashie::Extensions::DeepFind
-      puts("\n\nTopic:\t#{the_req_body["topic"]}\nReference:\t#{JSON.parse(the_req_body.deep_select("reference").to_s).join(', ')}\nMSISDN:\t#{JSON.parse(the_req_body.deep_select("sender_msisdn").to_s).join(', ')}\nAmount:\t#{JSON.parse(the_req_body.deep_select("amount").to_s).join(', ')}\nCurrency:\t#{JSON.parse(the_req_body.deep_select("currency").to_s).join(', ')}\nTill Number:\t#{JSON.parse(the_req_body.deep_select("till_number").to_s).join(', ')}\nSystem:\t#{JSON.parse(the_req_body.deep_select("system").to_s).join(', ')}\nSender First Name:\t#{JSON.parse(the_req_body.deep_select("sender_first_name").to_s).join(', ')}\nSender Middle Name:\t#{JSON.parse(the_req_body.deep_select("sender_middle_name").to_s).join(', ')}\nSender Last Name:\t#{JSON.parse(the_req_body.deep_select("sender_last_name").to_s).join(', ')}\n")
+      puts("\nTopic:\t#{the_req_body["topic"]}\nReference:\t#{JSON.parse(the_req_body.deep_select("reference").to_s).join(', ')}\nMSISDN:\t#{JSON.parse(the_req_body.deep_select("sender_msisdn").to_s).join(', ')}\nAmount:\t#{JSON.parse(the_req_body.deep_select("amount").to_s).join(', ')}\nCurrency:\t#{JSON.parse(the_req_body.deep_select("currency").to_s).join(', ')}\nTill Number:\t#{JSON.parse(the_req_body.deep_select("till_number").to_s).join(', ')}\nSystem:\t#{JSON.parse(the_req_body.deep_select("system").to_s).join(', ')}\nSender First Name:\t#{JSON.parse(the_req_body.deep_select("sender_first_name").to_s).join(', ')}\nSender Middle Name:\t#{JSON.parse(the_req_body.deep_select("sender_middle_name").to_s).join(', ')}\nSender Last Name:\t#{JSON.parse(the_req_body.deep_select("sender_last_name").to_s).join(', ')}\n")
     end
   end
 end
--- lib/k2_connect_ruby_api_gem/version.rb
+++ lib/k2_connect_ruby_api_gem/version.rb
@@ -1,3 +1,3 @@
 module K2ConnectRubyApiGem
-  VERSION = "0.7.29"
+  VERSION = "0.7.30"
 end
