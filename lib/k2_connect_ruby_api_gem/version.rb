f0db0c69b855a0666c890197ca243b2e070af212
DavidKar1uk1 <David.mwangi@kopokop.com>
2019-01-25 12:13:56 +0300

    Setup Test.

--- lib/k2_connect_ruby_api_gem/version.rb
+++
@@ -1,3 +0,0 @@
-module K2ConnectRubyApiGem
-  VERSION = "0.8.0"
-end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 require "bundler/setup"
-require "k2_connect_ruby_api_gem"
+require "k2_connect_ruby"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
