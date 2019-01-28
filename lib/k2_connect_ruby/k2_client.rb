0053bd28e84ef0752f024e14d3a6d9c9a507adc7
DavidKar1uk1 <David.mwangi@kopokop.com>
2019-01-28 11:34:32 +0300

    Version 0.8.18. Full update.

--- lib/k2_connect_ruby/k2_client.rb
+++ lib/k2_connect_ruby/k2_client.rb
@@ -15,9 +15,8 @@ module K2ConnectRuby
 
     # Method for parsing the Entire Request. Come back to it later to trim. L8r call it set_client_variables
     def parse_request(the_request)
-      # The Response Body
-      # @hash_body = Yajl::Parser.parse(the_request.body.string.to_json)
-      @hash_body_as = Yajl::Parser.parse(the_request.body.string.as_json)
+      # The Response Body.
+      @hash_body = Yajl::Parser.parse(the_request.body.string.as_json)
       # The Response Header
       @hash_header = Yajl::Parser.parse(the_request.headers.env.select{|k, _| k =~ /^HTTP_/}.to_json)
       # The K2 Signature
