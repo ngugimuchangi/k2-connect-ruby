be602e6da9fe950a74e4ca41e73202435ca76adc
DavidKar1uk1 <David.mwangi@kopokop.com>
2019-01-28 10:44:56 +0300

    Version 0.8.13. Onto Splitting the body.

--- lib/k2_connect_ruby/k2_authorize.rb
+++ lib/k2_connect_ruby/k2_authorize.rb
@@ -4,11 +4,7 @@ module K2ConnectRuby
     def authenticate(body, api_secret_key, signature)
       digest = OpenSSL::Digest.new('sha256')
       hmac = OpenSSL::HMAC.hexdigest(digest, api_secret_key, body)
-      # @truth = secure_compare(hmac, signature)
-      # @truth = hmac.to_s.eql?(signature)
-      # return secure_compare(hmac, signature)
-      # puts("\n\nThe HMAC:\t#{hmac}\nThe Signature:\t#{signature}")
-      puts("\n\nThe Secure Compare:\t#{ActiveSupport::SecurityUtils.secure_compare(hmac, signature)}\nThe To Eql:\t#{hmac.to_s.eql?(signature)}")
+      return ActiveSupport::SecurityUtils.secure_compare(hmac, signature)
     end
   end
 end
