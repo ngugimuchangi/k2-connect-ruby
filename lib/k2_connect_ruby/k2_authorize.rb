90b516ae838f5fb749ad194aa1fe16d4490d1c8e
DavidKar1uk1 <David.mwangi@kopokop.com>
2019-01-28 11:26:49 +0300

    Version 0.8.17. Changed to_json to as_json.

--- lib/k2_connect_ruby/k2_authorize.rb
+++ lib/k2_connect_ruby/k2_authorize.rb
@@ -3,7 +3,7 @@ module K2ConnectRuby
     # Compares HMAC signature with the key. Later call it K2Authenticator
     def authenticate(body, api_secret_key, signature)
       digest = OpenSSL::Digest.new('sha256')
-      hmac = OpenSSL::HMAC.hexdigest(digest, api_secret_key, body)
+      hmac = OpenSSL::HMAC.hexdigest(digest, api_secret_key, body.to_json)
       return ActiveSupport::SecurityUtils.secure_compare(hmac, signature)
     end
   end
