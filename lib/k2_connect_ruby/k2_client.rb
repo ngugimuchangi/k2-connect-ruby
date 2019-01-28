4d46738f5e986862033e9677af484c96657d2620
DavidKar1uk1 <David.mwangi@kopokop.com>
2019-01-28 09:30:05 +0300

    Version 0.8.4. Test for Class elements.

--- lib/k2_connect_ruby/k2_client.rb
+++ lib/k2_connect_ruby/k2_client.rb
@@ -15,12 +15,12 @@ module K2ConnectRuby
     # Method for parsing the Entire Request. Come back to it later to trim. L8r call it set_client_variables
     def parse_request(the_request)
       # The Response Body
-      # @hash_body = Yajl::Parser.parse(the_request.body.string.to_json)
+      @hash_body = Yajl::Parser.parse(the_request.body.string.to_json)
       # The Response Header
-      # @hash_header = Yajl::Parser.parse(the_request.headers.env.select{|k, _| k =~ /^HTTP_/}.to_json)
+      @hash_header = Yajl::Parser.parse(the_request.headers.env.select{|k, _| k =~ /^HTTP_/}.to_json)
       # The K2 Signature
-      # @k2_signature = @hash_header["HTTP_X_KOPOKOPO_SIGNATURE"]
-      puts(the_request)
+      @k2_signature = @hash_header["HTTP_X_KOPOKOPO_SIGNATURE"]
+      puts("\n\n#{the_request}\nHash Body:\t#{@hash_body}\nSignature:\t#{@k2_signature}")
     end
   end
 end
