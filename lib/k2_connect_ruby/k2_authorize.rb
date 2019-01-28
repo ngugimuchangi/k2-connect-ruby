dfcacc98009786e2859bf30c27b80eebe20e8e49
DavidKar1uk1 <David.mwangi@kopokop.com>
2019-01-28 09:58:49 +0300

    Version 0.8.7. Test for K2_Authorize.

--- lib/k2_connect_ruby/k2_authorize.rb
+++ lib/k2_connect_ruby/k2_authorize.rb
@@ -2,12 +2,13 @@ module K2ConnectRuby
   class K2Authorize
     # Compares HMAC signature with the key. Later call it K2Authenticator
     def authenticate(body, api_secret_key, signature)
-      digest = OpenSSL::Digest.new('sha256')
-      hmac = OpenSSL::HMAC.hexdigest(digest, api_secret_key, body)
+      # digest = OpenSSL::Digest.new('sha256')
+      # hmac = OpenSSL::HMAC.hexdigest(digest, api_secret_key, body)
       # @truth = secure_compare(hmac, signature)
       # @truth = hmac.to_s.eql?(signature)
       # return secure_compare(hmac, signature)
-      puts("\n\nThe HMAC:\t#{hmac}\nThe Signature:\t#{signature}")
+      # puts("\n\nThe HMAC:\t#{hmac}\nThe Signature:\t#{signature}")
+      puts("\n\nThe body:\t#{body}\nThe Secret Key:\t#{api_secret_key}\nThe Signature:\t#{signature}")
     end
   end
 end
