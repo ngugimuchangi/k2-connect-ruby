5025ec17275a590cbe4f3450ee906cef7d697199
DavidKar1uk1 <David.mwangi@kopokop.com>
2019-02-12 12:09:51 +0300

    Test Authenticator, body and api_secret_key .nil?.

--- lib/k2-connect-ruby/k2_authorize.rb
+++ lib/k2-connect-ruby/k2_authorize.rb
@@ -2,7 +2,7 @@ module K2ConnectRuby
   class K2Authenticator
     # Compares HMAC signature with the key. Later call it K2Authenticator
     def authenticate?(body, api_secret_key, signature)
-      raise K2NilAuthArgument if body.nil?
+      raise K2NilAuthArgument if body.nil? || api_secret_key.nil?
       digest = OpenSSL::Digest.new('sha256')
       hmac = OpenSSL::HMAC.hexdigest(digest, api_secret_key, body.to_json)
       return ActiveSupport::SecurityUtils.secure_compare(hmac, signature)
