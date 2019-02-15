fd903545e14d089faaedc834a9e53399e4e15a2f
DavidKar1uk1 <David.mwangi@kopokop.com>
2019-02-15 12:33:54 +0300

    Version 0.10.0. Code Reuse. Reusing the Connection Code. Using HTTP Persistent to reduce page load times.

--- lib/k2-connect-ruby.rb
+++ lib/k2-connect-ruby.rb
@@ -7,14 +7,79 @@ require "k2-connect-ruby/k2_errors"
 require "k2-connect-ruby/version"
 require "k2-connect-ruby/k2_stk"
 require "k2-connect-ruby/k2_pay"
-# require 'active_record'
+require 'net/http/persistent'
 require 'net/http'
-require "openssl"
+require 'openssl'
 require "yajl"
 require "json"
 require 'uri'
 
 
 module K2ConnectRuby
-  # Your code goes here...
+  # Method for sending the request to K2 sandbox or Mock Server (Receives the access_token)
+  def K2ConnectRuby.to_connect(params, path_url, access_token, is_subscribe, is_get_request)
+    # The Server
+    @postman_k2_mock_server = "https://a54fac07-5ac2-4ee2-8fcb-e3d5ac3ba8b1.mock.pstmn.io"
+
+    # The HTTP Exceptions
+    @http_exceptions = [ Errno::EINVAL, Errno::ECONNRESET, EOFError, Net::HTTPBadResponse,
+                         Errno::EHOSTUNREACH, Net::ProtocolError, Net::OpenTimeout, Net::HTTPFatalError,
+                         Net::HTTPHeaderSyntaxError, Net::HTTPServerException, OpenSSL::SSL::SSLError,
+                         Net::HTTP::Persistent::Error, Net::HTTPRetriableError ]
+
+    # Raised in the scenario that when requests for an access_token even though they already have one
+    if path_url.match?("ouath")
+      raise K2RepeatTokenRequest.new unless access_token.nil?
+    else
+      raise K2NilAccessToken.new if access_token.nil?
+    end
+    k2_url = URI.parse("#{@postman_k2_mock_server}#{path_url}")
+    k2_https = Net::HTTP::Persistent.new
+    if is_get_request
+      k2_request = Net::HTTP::Get.new(k2_url.request_uri)
+    else
+      k2_request = Net::HTTP::Post.new(k2_url.path)
+    end
+    if path_url.match?("ouath")
+      k2_request.add_field('Content-Type', 'application/vnd.kopokopo.v4.hal+json')
+    else
+      k2_request.add_field('Content-Type', 'application/vnd.kopokopo.v4.hal+json')
+      k2_request.add_field('Accept', 'application/vnd.kopokopo.v4.hal+json')
+      k2_request.add_field("Authorization", "Bearer #{access_token}")
+    end
+    k2_request.body = params
+
+    begin
+      if is_get_request
+        @k2_response = k2_https.request(k2_request)
+      else
+        @k2_response = k2_https.request(k2_url, k2_request)
+      end
+      # @k2_response = k2_https.request(k2_request)
+    rescue Net::HTTP::Persistent::Error => e
+      puts(e.message)
+    rescue K2RepeatTokenRequest => k2
+      puts(k2.message)
+    rescue K2NilAccessToken => k3
+      puts(k3.message)
+    rescue StandardError => se
+      puts(se.message)
+      return false
+    end
+
+    puts("\nThe Response:\t#{@k2_response.body.to_s}")
+    # Add a method to fetch all the components of the response
+    if path_url.match?("ouath")
+      access_token = Yajl::Parser.parse(@k2_response.body)["access_token"]
+      puts("\nThe Access Token:\t#{access_token}")
+      return access_token
+    else
+      unless is_subscribe
+        @k2_stk_location = Yajl::Parser.parse(@k2_response.body)["location"]
+        puts("\nThe Location Url:\t#{@k2_stk_location}")
+      end
+    end
+    k2_https.shutdown
+    return true
+  end
 end
--- lib/k2-connect-ruby/k2_transfer.rb
+++ lib/k2-connect-ruby/k2_transfer.rb
@@ -1,108 +1,55 @@
-module K2ConnectRuby
-  class K2Transfer
-    attr_accessor :k2_response_transfer
+class K2Transfer
+  attr_accessor :k2_response_transfer,
+                :k2_access_token
 
-    # Sets the Url for the mock server / API server
-    def set_k2_mocks
-      @postman_k2_mock_server = "https://a54fac07-5ac2-4ee2-8fcb-e3d5ac3ba8b1.mock.pstmn.io"
-    end
+  def initialize(access_token)
+    @k2_access_token = access_token
+  end
 
-    # Create a Verified Settlement Account via API
-    def settlement_account(transfer_params)
-      set_k2_mocks
-      k2_url = URI.parse("#{@postman_k2_mock_server}/merchant_bank_accounts")
-      k2_https = Net::HTTP.new(k2_url.host, k2_url.port)
-      k2_https.use_ssl =true
-      k2_https.verify_mode =OpenSSL::SSL::VERIFY_PEER
-      k2_request =Net::HTTP::Post.new(k2_url)
-      k2_request.add_field('Content-Type', 'application/vnd.kopokopo.v4.hal+json')
-      k2_request.add_field('Accept', 'application/vnd.kopokopo.v4.hal+json')
-      k2_request.add_field('Authorization', "Bearer access_token")
+  # Create a Verified Settlement Account via API
+  def settlement_account(transfer_params)
+    # The Request Body Parameters
+    settlement_body = {
+        "account_name": "#{transfer_params["account_name"]}",
+        "bank_ref": "#{transfer_params["bank_ref"]}",
+        "bank_branch_ref": "#{transfer_params["bank_branch_ref"]}",
+        "account_number": "#{transfer_params["account_number"]}"
+    }.to_json
+    K2ConnectRuby.to_connect(settlement_body, "/merchant_bank_accounts", @k2_access_token, false, false)
+  rescue StandardError => e
+    puts(e.message)
+    return false
+  end
 
-      k2_request.body = {
-          "account_name": "#{transfer_params["account_name"]}",
-          "bank_ref": "#{transfer_params["bank_ref"]}",
-          "bank_branch_ref": "#{transfer_params["bank_branch_ref"]}",
-          "account_number": "#{transfer_params["account_number"]}"
+  # Create a either a 'blind' transfer, for when destination is specified, and a 'targeted' transfer which has a specified destination.
+  def transfer_funds(destination, transfer_params)
+    # The Request Body Parameters
+    if destination.nil?
+      # Blind Transfer
+      transfer_body = {
+          "amount": {
+              "currency": "#{transfer_params["currency"]}",
+              "value": "#{transfer_params["value"]}"
+          }
       }.to_json
-      begin
-        @k2_response_transfer = k2_https.request(k2_request)
-      rescue Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError, Net::HTTPBadResponse,
-          Net::HTTPHeaderSyntaxError, Net::ProtocolError => e
-        puts(e.message)
-      end
-      # @k2_response_transfer = k2_https.request(k2_request)
-      puts("\nThe Response:\t#{k2_response_transfer.body.to_s}")
-      @k2_stk_location = Yajl::Parser.parse(k2_response_transfer.body)["location"]
-      puts("\nThe Location Url:\t#{@k2_stk_location}")
-      return true
-    rescue StandardError => e
-      puts(e.message)
-      return false
-    end
-
-    # Create a either a 'blind' transfer, for when destination is specified, and a 'targeted' transfer which has a specified destination.
-    def transfer_funds(destination, transfer_params)
-      set_k2_mocks
-      k2_url = URI.parse("#{@postman_k2_mock_server}/transfers")
-      k2_https = Net::HTTP.new(k2_url.host, k2_url.port)
-      k2_https.use_ssl =true
-      k2_https.verify_mode =OpenSSL::SSL::VERIFY_PEER
-      k2_request =Net::HTTP::Post.new(k2_url)
-      k2_request.add_field('Content-Type', 'application/vnd.kopokopo.v4.hal+json')
-      k2_request.add_field('Accept', 'application/vnd.kopokopo.v4.hal+json')
-      k2_request.add_field('Authorization', "Bearer access_token")
-      if destination.nil?
-        # Blind Transfer
-        k2_request.body = {
-            "amount": {
-                "currency": "#{transfer_params["currency"]}",
-                "value": "#{transfer_params["value"]}"
-            }
-        }.to_json
-      else
-        # Targeted Transfer
-        k2_request.body = {
-            "amount": {
-                "currency": "#{transfer_params["currency"]}",
-                "value": "#{transfer_params["value"]}"
-            },
-            "destination": "#{destination}"
-        }.to_json
-      end
-      begin
-        @k2_response_transfer = k2_https.request(k2_request)
-      rescue Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError, Net::HTTPBadResponse,
-          Net::HTTPHeaderSyntaxError, Net::ProtocolError => e
-        puts(e.message)
-      end
-      # @k2_response_transfer = k2_https.request(k2_request)
-      puts("\nThe Response:\t#{@k2_response_transfer.body.to_s}")
-      @k2_stk_location = Yajl::Parser.parse(@k2_response_transfer.body)["location"]
-      puts("\nThe Location Url:\t#{@k2_stk_location}")
-    end
-
-    # Check the status of a prior initiated Transfer.
-    def query_transfer(id)
-      set_k2_mocks
-      k2_url = URI.parse("#{@postman_k2_mock_server}/transfers")
-      k2_https = Net::HTTP.new(k2_url.host, k2_url.port)
-      k2_https.use_ssl =true
-      k2_https.verify_mode =OpenSSL::SSL::VERIFY_PEER
-      k2_request =Net::HTTP::Get.new(k2_url)
-      k2_request.add_field('Accept', 'application/vnd.kopokopo.v4.hal+json')
-      k2_request.add_field('Authorization', "Bearer access_token")
-      k2_request.body = {
-          "ID": "#{id}"
+    else
+      # Targeted Transfer
+      transfer_body = {
+          "amount": {
+              "currency": "#{transfer_params["currency"]}",
+              "value": "#{transfer_params["value"]}"
+          },
+          "destination": "#{destination}"
       }.to_json
-      begin
-        @k2_response_transfer = k2_https.request(k2_request)
-      rescue Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError, Net::HTTPBadResponse,
-          Net::HTTPHeaderSyntaxError, Net::ProtocolError => e
-        puts(e.message)
-      end
-      # @k2_response_transfer = k2_https.request(k2_request)
-      puts("\nThe Response:\t#{@k2_response_transfer.body.to_s}")
     end
+    K2ConnectRuby.to_connect(transfer_body, "/transfers", @k2_access_token, false, false)
+  end
+
+  # Check the status of a prior initiated Transfer.
+  def query_transfer(id)
+    query_body = {
+        "ID": "#{id}"
+    }.to_json
+    K2ConnectRuby.to_connect(query_body, "/transfers", @k2_access_token, false, true)
   end
 end
