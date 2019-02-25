00ddce49f4d0db05b5cfba2bdcb6536bbf51bbf0
DavidKar1uk1 <David.mwangi@kopokopo.com>
2019-02-25 16:07:54 +0300

    K2Split edit.

--- lib/k2-connect-ruby/k2_modules/k2_split.rb
+++ lib/k2-connect-ruby/k2_modules/k2_split.rb
@@ -51,7 +51,6 @@ module K2Split
   end
 
   def self.return_hash(the_body, number = 0, instance_hash={}, obj)
-    components(the_body)
     while number < obj.instance_variables.length
       obj.instance_variables.each do |value|
         instance_hash[:"#{value.to_s.tr('@', '')}"] = obj.instance_variable_get(value)
