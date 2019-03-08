7fb5ce6afd131c6469406edecf945906937d01f3
DavidKar1uk1 <David.mwangi@kopokopo.com>
2019-03-08 17:31:22 +0300

    Version 0.9.35. Validation.

--- lib/k2-connect-ruby/k2_modules/k2_validation.rb
+++ lib/k2-connect-ruby/k2_modules/k2_validation.rb
@@ -29,19 +29,19 @@ module K2Validation
   # Validate the Hash Input Parameters
   def validate_hash(the_input, empty_keys = Array.new, invalid_keys = HashWithIndifferentAccess.new, the_array)
     nil_params(the_input, empty_keys)
-    if empty_keys.blank?
+    if empty_keys.present?
+      puts "You have Blank Parameter/Hash Keys"
+      raise K2InvalidHash.new(empty_keys)
+    else
       puts "No Blank Parameter/Hash Keys"
       incorrect_keys(the_input, invalid_keys, the_array)
-      unless invalid_keys.blank?
+      if invalid_keys.present?
         puts "You have Incorrect Parameter/Hash Keys"
         puts "List\t#{invalid_keys}"
         raise IncorrectParams.new(invalid_keys)
       end
       puts "No Incorrect Parameter/Hash Keys"
       true
-    else
-      puts "You have Blank Parameter/Hash Keys"
-      raise K2InvalidHash.new(empty_keys)
     end
   end
 
