bd6248e4338276c5c14749c74ea272f9bfce6939
DavidKar1uk1 <David.mwangi@kopokopo.com>
2019-03-08 17:05:01 +0300

    Version 0.9.32. Changed to Hash.

--- lib/k2-connect-ruby/k2_modules/k2_process.rb
+++ lib/k2-connect-ruby/k2_modules/k2_process.rb
@@ -13,53 +13,50 @@ module K2ProcessResult
       puts "Buy Goods Transaction Received."
       buy_goods = BuyGoods.new
       buy_goods.components(the_body)
-      K2ProcessResult.return_obj_array(buy_goods)
+      K2ProcessResult.return_obj_hash(buy_goods)
     when "buygoods_transaction_reversed"
       puts "Buy Goods Transaction Reversed."
       reversals = Reversal.new
       reversals.components(the_body)
-      K2ProcessResult.return_obj_array(reversals)
+      K2ProcessResult.return_obj_hash(reversals)
     when "settlement_transfer_completed"
       puts "Settlement Transaction."
       settlement = Settlement.new
       settlement.components(the_body)
-      K2ProcessResult.return_obj_array(settlement)
+      K2ProcessResult.return_obj_hash(settlement)
     when "customer_created"
       puts "Customer Created."
       customer = CustomerCreated.new
       customer.components(the_body)
-      K2ProcessResult.return_obj_array(customer)
+      K2ProcessResult.return_obj_hash(customer)
     when "payment_request"
       puts "STK Push Payment Request Result."
       stk_result = K2ProcessStk.new
       stk_result.components(the_body)
-      K2ProcessResult.return_obj_array(stk_result)
+      K2ProcessResult.return_obj_hash(stk_result)
     when "pay_result"
       puts "PAY Payment Request Result."
       pay_result = K2ProcessPay.new
       pay_result.components(the_body)
-      K2ProcessResult.return_obj_array(pay_result)
+      K2ProcessResult.return_obj_hash(pay_result)
     else
       raise ArgumentError.new("No Other Specified Event!")
     end
   end
 
   # TODO, Identify which is better, Hash, or Array
-  # def self.return_obj_hash(number = 0, instance_hash=HashWithIndifferentAccess.new, obj)
-  #   while number < obj.instance_variables.length
-  #     obj.instance_variables.each do |value|
-  #       instance_hash[:"#{value.to_s.tr('@', '')}"] = obj.instance_variable_get(value)
-  #       number+=1
-  #     end
-  #   end
-  #   return instance_hash
-  # end
-
-  def self.return_obj_array(instance_array=Array.new, obj)
+  def self.return_obj_hash(instance_hash=HashWithIndifferentAccess.new, obj)
     obj.instance_variables.each do |value|
-      instance_array << obj.instance_variable_get(value)
+      instance_hash[:"#{value.to_s.tr('@', '')}"] = obj.instance_variable_get(value)
     end
-    return instance_array
+    instance_hash
   end
+
+  # def self.return_obj_array(instance_array=Array.new, obj)
+  #   obj.instance_variables.each do |value|
+  #     instance_array << obj.instance_variable_get(value)
+  #   end
+  #   return instance_array
+  # end
 
 end
--- lib/k2-connect-ruby/version.rb
+++ lib/k2-connect-ruby/version.rb
@@ -1,3 +1,3 @@
 module K2ConnectRuby
-  VERSION = "0.10.32"
+  VERSION = "0.10.33"
 end
