5cc4663114f547e2e3f88ed16e4193361205c4e7
DavidKar1uk1 <David.mwangi@kopokopo.com>
2019-03-15 12:02:00 +0300

    Version 0.10.2. Replacing double quotes with single quotes concerning strings that do not have a need for interpolation.

--- lib/k2-connect-ruby/k2_services/client/k2_client.rb
+++ lib/k2-connect-ruby/k2_services/client/k2_client.rb
@@ -7,28 +7,21 @@ class K2Client
 
   # Initialize method
   def initialize(api_secret_key)
-    raise ArgumentError.new("No Secret Key Given!") if api_secret_key.blank?
-
+    raise ArgumentError.new('No Secret Key Given!') if api_secret_key.blank?
     @api_secret_key = api_secret_key
   end
 
   # Method for parsing the Entire Request. Come back to it later to trim. L8r call it set_client_variables
   def parse_request(the_request)
-    raise ArgumentError.new("Nil Request Parameter Input!") if the_request.blank?
+    raise ArgumentError.new('Nil Request Parameter Input!') if the_request.blank?
 
     # The Response Body.
     @hash_body = Yajl::Parser.parse(the_request.body.string.as_json)
     # The Response Header
     hash_header = Yajl::Parser.parse(the_request.env.select { |k, _| k =~ /^HTTP_/ }.to_json)
     # The K2 Signature
-    @k2_signature = hash_header["HTTP_X_KOPOKOPO_SIGNATURE"]
+    @k2_signature = hash_header['HTTP_X_KOPOKOPO_SIGNATURE']
     # if the_request.scheme.eql?("https")
-    #   # The Response Body.
-    #   @hash_body = Yajl::Parser.parse(the_request.body.string.as_json)
-    #   # The Response Header
-    #   @hash_header = Yajl::Parser.parse(the_request.headers.env.select{|k, _| k =~ /^HTTP_/}.to_json)
-    #   # The K2 Signature
-    #   @k2_signature = @hash_header["HTTP_X_KOPOKOPO_SIGNATURE"]
     # else
     #   raise K2InsecureRequest.new
     # end
