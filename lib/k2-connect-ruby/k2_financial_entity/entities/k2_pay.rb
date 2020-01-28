d3be023789e39549dacbbca0db13d3041ae24c1d
DavidKar1uk1 <david.mwangi@kopokopo.com>
2020-01-28 10:44:48 +0300

    K2Pay Validated Hash Input Keys
    Configured suitable keys for authenticating against adding mobile and bank pay recipients together with creating payments.

--- lib/k2-connect-ruby/k2_financial_entity/entities/k2_pay.rb
+++ lib/k2-connect-ruby/k2_financial_entity/entities/k2_pay.rb
@@ -6,33 +6,33 @@ class K2Pay < K2Entity
 
   # Adding PAY Recipients with either mobile_wallets or bank_accounts as destination of your payments.
   def pay_recipients(params)
-    # Validation
-    #params = validate_input(params, @exception_array += %w[first_name last_name phone email network pay_type currency value account_name bank_id bank_branch_id account_number])
-    params = validate_input(params, @exception_array += %w[type pay_recipient])
+    @exception_array += %w[first_name last_name phone email]
     # In the case of mobile pay
     # if params['pay_type'].eql?('mobile_wallet')
     if params[:type].eql?('mobile_wallet')
-      #puts "Phone: #{params[:pay_recipient][:phone]}"
-      #puts "Validated Phone: #{validate_phone(params[:pay_recipient][:phone])}"
+      # Validation
+      params = validate_input(params, @exception_array += %w[network])
       k2_request_pay_recipient = {
-        first_name: params[:pay_recipient][:first_name],
-        last_name: params[:pay_recipient][:last_name],
-        phone: validate_phone(params[:pay_recipient][:phone]),
-        email: validate_email(params[:pay_recipient][:email]),
-        network: params[:pay_recipient][:network]
+        first_name: params[:first_name],
+        last_name: params[:last_name],
+        phone: validate_phone(params[:phone]),
+        email: validate_email(params[:email]),
+        network: params[:network]
       }
       #puts "Pay Recipient: #{k2_request_pay_recipient}"
     #elsif params['pay_type'].eql?('bank_account')
     elsif params[:type].eql?('bank_account')
+      # Validation
+      params = validate_input(params, @exception_array += %w[account_name bank_id bank_branch_id account_number])
       # In the case of bank pay
       k2_request_pay_recipient = {
-        name: "#{params[:pay_recipient][:first_name]} #{params[:last_name]}",
-        account_name: params[:pay_recipient][:account_name],
-        bank_id: params[:pay_recipient][:bank_id],
-        bank_branch_id: params[:pay_recipient][:bank_branch_id],
-        account_number: params[:pay_recipient][:account_number],
-        email: validate_email(params[:pay_recipient][:email]),
-        phone: validate_phone(params[:pay_recipient][:phone])
+        name: "#{params[:first_name]} #{params[:last_name]}",
+        account_name: params[:account_name],
+        bank_id: params[:bank_id],
+        bank_branch_id: params[:bank_branch_id],
+        account_number: params[:account_number],
+        email: validate_email(params[:email]),
+        phone: validate_phone(params[:phone])
       }
     else
       raise ArgumentError, 'Undefined Payment Method.'
@@ -53,8 +53,8 @@ class K2Pay < K2Entity
   # Create an outgoing Payment to a third party.
   def create_payment(params)
     # Validation
-    #params = validate_input(params, @exception_array += %w[currency value])
-    params = validate_input(params, @exception_array += %w[destination amount metadata _links])
+    params = validate_input(params, @exception_array += %w[currency value])
+    # params = validate_input(params, @exception_array += %w[destination amount metadata _links])
     # The Request Body Parameters
     k2_request_pay_amount = {
       currency: params[:currency],
@@ -65,7 +65,7 @@ class K2Pay < K2Entity
       notes: 'Salary payment for May 2018'
     }
     k2_request_links = {
-        call_back_url: params[:callback_url]
+        callback_url: params[:callback_url]
     }
     create_payment_body = {
       destination: 'c7f300c0-f1ef-4151-9bbe-005005aa3747',
