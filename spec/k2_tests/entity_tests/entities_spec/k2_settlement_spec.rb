ec9f21d8951a7007396ed37e055012ce0785b1f7
DavidKar1uk1 <david.mwangi@kopokopo.com>
2020-10-26 10:56:30 +0300

    Add settlement method for merchant back accounts when adding a settlement account.

--- spec/k2_tests/entity_tests/entities_spec/k2_settlement_spec.rb
+++ spec/k2_tests/entity_tests/entities_spec/k2_settlement_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe K2Settlement do
     @k2settlement = K2Settlement.new(K2AccessToken.new('ReMYAYdLKcg--XNmKhzkLNTIbXPvOUPs3hyycUF8WmI', '4707e306330759f4b63716f0525f6634a4685b4b4bf75b3381f1244ee77eb3fa').request_token)
 
     @mobile_settle_account = HashWithIndifferentAccess.new(type: 'merchant_wallet', first_name: 'first_name', last_name: 'last_name', phone_number: '254716230902', network: 'Safaricom')
-    @bank_settle_account = HashWithIndifferentAccess.new(type: 'merchant_bank_account', account_name: 'account_name', bank_ref: 'bank_ref', bank_branch_ref: 'bank_branch_ref', account_number: 'account_number')
+    @bank_settle_account_eft = HashWithIndifferentAccess.new(type: 'merchant_bank_account', account_name: 'account_name', bank_ref: '2', bank_branch_ref: '633aa26c-7b7c-4091-ae28-96c0687cf886', account_number: 'account_number', settlement_method: "EFT")
+    @bank_settle_account_rts = HashWithIndifferentAccess.new(type: 'merchant_bank_account', account_name: 'account_name', bank_ref: '2', bank_branch_ref: '633aa26c-7b7c-4091-ae28-96c0687cf886', account_number: 'account_number', settlement_method: "RTS")
   end
 
   context "Mobile Wallet settlement account" do
@@ -36,11 +37,21 @@ RSpec.describe K2Settlement do
   end
 
   context "Bank Settlement Account" do
-    describe "#add_settlement_account" do
-      it 'should creating verified bank settlement account' do
-        SpecConfig.custom_stub_request('post', K2Config.path_url('settlement_bank_account'), @bank_settle_account, 200)
-        expect { @k2settlement.add_settlement_account(@bank_settle_account) }.not_to raise_error
-        expect(WebMock).to have_requested(:post, URI.parse(K2Config.path_url('settlement_bank_account')))
+    describe " #add_settlement_account" do
+      context "EFT settlement_method" do
+        it 'should create verified bank settlement account' do
+          SpecConfig.custom_stub_request('post', K2Config.path_url('settlement_bank_account'), @bank_settle_account_eft, 200)
+          expect { @k2settlement.add_settlement_account(@bank_settle_account_eft) }.not_to raise_error
+          expect(WebMock).to have_requested(:post, URI.parse(K2Config.path_url('settlement_bank_account')))
+        end
+      end
+
+      context "RTS settlement_method" do
+        it 'should create verified bank settlement account' do
+          SpecConfig.custom_stub_request('post', K2Config.path_url('settlement_bank_account'), @bank_settle_account_rts, 200)
+          expect { @k2settlement.add_settlement_account(@bank_settle_account_rts) }.not_to raise_error
+          expect(WebMock).to have_requested(:post, URI.parse(K2Config.path_url('settlement_bank_account')))
+        end
       end
     end
 
