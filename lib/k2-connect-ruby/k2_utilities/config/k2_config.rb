c5fb7305a2bf63cf5d63b3855ef7263aa4feafbf
DavidKar1uk1 <david.mwangi@kopokopo.com>
2020-11-02 12:41:42 +0300

    Test text file content.

--- lib/k2-connect-ruby/k2_utilities/config/k2_config.rb
+++ lib/k2-connect-ruby/k2_utilities/config/k2_config.rb
@@ -1,6 +1,7 @@
 # TODO: try to see if you can implement Environment variables
 module K2Config
-  @config = YAML.load_file(File.join('lib', 'k2-connect-ruby', 'k2_utilities', 'config', 'k2_config.txt')).with_indifferent_access
+  # @config = YAML.load_file(File.join('lib', 'k2-connect-ruby', 'k2_utilities', 'config', 'k2_config.txt')).with_indifferent_access
+  @config = File.open("lib/k2-connect-ruby/k2_utilities/config/k2_config.txt", 'r')
 
   # def yml_file_location(user_file_location)
   #   file_location = File.join('lib', 'k2-connect-ruby', 'k2_utilities', 'config', 'k2_config.yml') || user_file_location
